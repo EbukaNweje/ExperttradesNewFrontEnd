83b19b878e57fddda34adc9e8327125fba37c925
eflexTech <eflexmedia1@gmail.com>
2024-05-28 01:15:40 +0100

    g

---
+++ user/js/resetData.js
@@ -0,0 +1,37 @@
+
+const email = document.getElementById('email')
+const button = document.getElementById('forgetPwdBtn')
+console.log(button);
+
+// console.log(password);
+
+button.onclick = async (event) => {
+  event.preventDefault();
+
+  button.innerHTML= "loading...";
+
+  const data = {
+    email: email.value,
+  };
+
+  console.log(data);
+
+  fetch('https://experttradesnewbackend.onrender.com/api/forgotpassword', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response=> response.json())
+    .then(response => {
+      console.log(response)
+      alert(response.message)
+      window.location = "index.html"
+    })
+    .catch((error) => {
+      console.log(error);
+    button.innerHTML= "loading...";
+
+    });
+};
